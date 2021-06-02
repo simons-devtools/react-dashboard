4db4ddb92eb215d9e6646e16a040bd5e1977c791
“c-moX” <cmoxsh@gmail.com>
2021-06-02 08:15:51 +0600

    updated the single viewer page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,11 +1,70 @@
 import React from 'react';
-import SmartPhone from '../Products/Collection-1/SmartPhone';
+import Logo from '../assets/logo.svg';
 
 const Dashboard = (props) => {
     return (
-        <div className="App">
-            <h2>I'm the dashboard.</h2>
-            {/* <SmartPhone /> */}
+        <div>
+
+            <header style={{ display: 'flex', flexWrap: 'wrap', margin: '20px' }}>
+                <img src={Logo} style={{ height: '100px' }} />
+                <h1>Welcome to admin dashboard</h1>
+            </header>
+
+            <main style={{ margin: '20px' }}>
+                <div className="content-box">
+                    <h2>Component one content</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat unde reiciendis ea id! Voluptates aliquam saepe ea minima libero. Praesentium voluptatem possimus ad numquam impedit similique non officiis consequatur excepturi quae minima repellat quibusdam, eius debitis reprehenderit, nesciunt autem fugiat, assumenda quasi est omnis exercitationem quod! Dolor corrupti nobis rem!</p>
+                    <div>
+                        <button>Go to console</button>
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div className="content-box">
+                    <h2>Component two content</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat unde reiciendis ea id! Voluptates aliquam saepe ea minima libero. Praesentium voluptatem possimus ad numquam impedit similique non officiis consequatur excepturi quae minima repellat quibusdam, eius debitis reprehenderit, nesciunt autem fugiat, assumenda quasi est omnis exercitationem quod! Dolor corrupti nobis rem!</p>
+                    <div>
+                        <button>Go to console</button>
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div className="content-box">
+                    <h2>Component three content</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat unde reiciendis ea id! Voluptates aliquam saepe ea minima libero. Praesentium voluptatem possimus ad numquam impedit similique non officiis consequatur excepturi quae minima repellat quibusdam, eius debitis reprehenderit, nesciunt autem fugiat, assumenda quasi est omnis exercitationem quod! Dolor corrupti nobis rem!</p>
+                    <div>
+                        <button>Go to console</button>
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div className="content-box">
+                    <h2>Component four content</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat unde reiciendis ea id! Voluptates aliquam saepe ea minima libero. Praesentium voluptatem possimus ad numquam impedit similique non officiis consequatur excepturi quae minima repellat quibusdam, eius debitis reprehenderit, nesciunt autem fugiat, assumenda quasi est omnis exercitationem quod! Dolor corrupti nobis rem!</p>
+                    <div>
+                        <button>Go to console</button>
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div className="content-box">
+                    <h2>Component four content</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat unde reiciendis ea id! Voluptates aliquam saepe ea minima libero. Praesentium voluptatem possimus ad numquam impedit similique non officiis consequatur excepturi quae minima repellat quibusdam, eius debitis reprehenderit, nesciunt autem fugiat, assumenda quasi est omnis exercitationem quod! Dolor corrupti nobis rem!</p>
+                    <div>
+                        <button>Go to console</button>
+                        <button>Read more</button>
+                    </div>
+                </div>
+                <div className="content-box">
+                    <h2>Component four content</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat unde reiciendis ea id! Voluptates aliquam saepe ea minima libero. Praesentium voluptatem possimus ad numquam impedit similique non officiis consequatur excepturi quae minima repellat quibusdam, eius debitis reprehenderit, nesciunt autem fugiat, assumenda quasi est omnis exercitationem quod! Dolor corrupti nobis rem!</p>
+                    <div>
+                        <button>Go to console</button>
+                        <button>Read more</button>
+                    </div>
+                </div>
+            </main>
+
+            <footer style={{ color: 'azure', backgroundColor: 'grey', padding: '14px', textAlign: 'center' }}>
+                <p>&copy; Copy right by 2021 || All right reserved by Devtools.</p>
+            </footer>
+
         </div>
     );
 }
--- src/Pages/UserControl.jsx
+++ src/Pages/UserControl.jsx
@@ -2,8 +2,62 @@ import React from 'react';
 
 const UserControl = (props) => {
     return (
-        <div>
-            <h2>I am users control.</h2>
+        <div className="main-profile">
+
+            <div class="profile-left">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
+                <ul>
+                    <li style={{ fontWeight: 'bold' }}>Login account status</li>
+                    <li>simonhembrom</li>
+                    <li>cmoxsh@gmail.com</li>
+                    <li>pass: abcd1234</li>
+                    <li>active: 02/06/2021</li>
+                </ul>
+            </div>
+
+            <div className="profile-right">
+                <div className="content-top">
+                    <div className="">
+                        <h2>Mr. simon hembrom</h2>
+                        <h3 style={{ color: 'blueviolet' }}>Web Developer and Designer</h3>
+                        <p>Complete order: 8/10</p>
+                    </div>
+                    <div className="">
+                        <button>Edit Profile</button>
+                    </div>
+                </div>
+
+                <div>
+                    <h3 style={{ borderBottom: '1px solid #7270701e' }}>User basic shipment information</h3>
+                    <div className="tween-content">
+                        <div className="user-title">
+                            <ul>
+                                <li>User ID</li>
+                                <li>First name</li>
+                                <li>Last name</li>
+                                <li>Username</li>
+                                <li>Email address</li>
+                                <li>Phone no</li>
+                                <li>Address</li>
+                                <li>User status</li>
+                            </ul>
+                        </div>
+                        <div className="user-info">
+                            <ul>
+                                <li>df4564dfd547df125jh5</li>
+                                <li>Mr. simon</li>
+                                <li>hembrom</li>
+                                <li>simonhembrom</li>
+                                <li>simonhembrom.dev@gmail.com</li>
+                                <li>+880 1799867752</li>
+                                <li>Salna-bazar, Gazipur-shadar, Dhaka, BD</li>
+                                <li style={{ color: 'greenyellow' }}>Active</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
